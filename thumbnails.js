66f74b9dcfb339e8651903d4538fd11728b38a63
App Academy Student <student@appacademy.io>
2015-05-28 21:51:09 -0700

    Stop repeated image selection, speed up flashing

--- thumbnails.js
+++ thumbnails.js
@@ -26,7 +26,7 @@ $.Thumbnails = function(el) {
     this.fillGutterImages();
   }.bind(this));
 
-  setInterval(this.randomImage.bind(this), Math.floor(Math.random() * 200 + 100));
+  setInterval(this.randomImage.bind(this), Math.floor(Math.random() * 100 + 50));
 };
 
 $.Thumbnails.prototype.fillGutterImages = function () {
@@ -37,8 +37,12 @@ $.Thumbnails.prototype.fillGutterImages = function () {
   });
 };
 
-$.Thumbnails.prototype.randomImage = function (first_argument) {
-  var activeImgIdx = Math.floor(Math.random() * this.$images.length);
+$.Thumbnails.prototype.randomImage = function () {
+  var oldImgIdx = this.$images.index(this.$activeImg);
+  var activeImgIdx = oldImgIdx;
+  while (activeImgIdx === oldImgIdx) {
+    activeImgIdx = Math.floor(Math.random() * this.$images.length);
+  }
   this.$activeImg = this.$images.eq(activeImgIdx);
   this.activate(this.$activeImg);
 };
